0fbad27837f393ac0ffe4ffaf17286bda4ad9a07
Jonathan Metz <jcmetz21@gmail.com>
2018-01-28 18:48:39 -0800

    :zap: Complete app.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # number_fact_alexa_skill
 Number fact alexa skill. Can choose a specific number or let alexa decide for you. 
+
+To run:
+1. Run flask app.
+2. Open a secure tunnel to localhost and expose that tunnel behind an http endpoint: ./ngrok http 5000
+3. Paste endpoint in appropriate field on amazon developer site.
---
+++ numbergeek.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question
+import requests
+import logging
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+
+@ask.launch
+def launch():
+	welcome_text = render_template('welcome')
+	reprompt_text = render_template('welcome_reprompt')
+	return question(welcome_text).reprompt(reprompt_text)
+
+@ask.intent('RandomNumberFactIntent')
+def random_fact():
+	# logging.getLogger('flask_ask').debug('RANDOM NUMBER')
+	url = 'http://numbersapi.com/random/trivia'
+	r = requests.get(url)
+	fact = r.text
+	return statement(speech=fact).simple_card(title='Number Facts', content=fact)
+
+@ask.intent('SpecifiedNumberFactIntent', convert={'number': int})
+def specified_fact(number):
+	# logging.getLogger('flask_ask').debug(f'SPECIFIC NUMBER: {number} {type(number)}')
+	if number is None:
+		repeat_text = render_template('number_error')
+		return question(repeat_text).reprompt(repeat_text)
+	url = f'http://numbersapi.com/{number}'
+	r = requests.get(url)
+	fact = r.text
+	return statement(speech=fact).simple_card(title='Number Facts', content=fact)
+
+@ask.intent('AMAZON.StopIntent')
+def stop():
+	return statement(render_template('bye'))
+
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+	return statement(render_template('bye'))
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+	return question(render_template('help'))
+
+@ask.session_ended
+def session_ended():
+	return '{}', 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
